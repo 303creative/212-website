df831002e5f09ca635a828cea366e70ca9942920
The303 <maikelmarshall07@gmail.com>
2026-09-12 03:47:25 -0400

    ok

---
+++ sw.js
@@ -0,0 +1,13 @@
+const CACHE='212-v1';
+self.addEventListener('install', e => { self.skipWaiting(); });
+self.addEventListener('activate', e => e.waitUntil(self.clients.claim()));
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    fetch(e.request).then(r => {
+      const copy = r.clone();
+      caches.open(CACHE).then(c => c.put(e.request, copy)).catch(()=>{});
+      return r;
+    }).catch(() => caches.match(e.request))
+  );
+});
